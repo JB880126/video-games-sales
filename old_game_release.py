6d57b090c83793a754697823b7dc7f821186f5af
tdljayden <110114652+tdljayden@users.noreply.github.com>
2024-04-25 09:37:02 +0100

    Remove "modules imported" messages

--- old_game_release.py
+++ old_game_release.py
@@ -4,7 +4,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import warnings
 warnings.filterwarnings("ignore")
-print("The modules are imported")
 
 def visualise_games_old_releases(df):
 
